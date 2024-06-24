758e671d4ca1da94001c1c172e03a280f2a8399f
박원빈 <harry21@kakao.com>
2024-06-24 14:25:51 +0900

    init project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import Providers from "./Providers.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Providers>
+    <App />
+  </Providers>
+);
