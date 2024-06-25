36dd5f578b111df195685382d6a036be6feae52f
박원빈 <harry21@kakao.com>
2024-06-25 15:06:34 +0900

    (add) select country logic

--- src/components/country/CountryItem.tsx
+++ src/components/country/CountryItem.tsx
@@ -21,9 +21,15 @@ export const CountryItemSkeleton: React.FC = () => {
 
 interface CountryItemProps {
   country: CountryResponse;
+  isSelectedCountry: boolean;
+  handleSelectCountry: (newCountry: CountryResponse) => void;
 }
 
-const CountryItem: React.FC<CountryItemProps> = ({ country }) => {
+const CountryItem: React.FC<CountryItemProps> = ({
+  country,
+  isSelectedCountry,
+  handleSelectCountry,
+}) => {
   const { data: imageUrl } = useSuspenseQuery({
     queryKey: ["img", country.flags.png],
     queryFn: () => fetchImage(country.flags.png),
@@ -31,7 +37,10 @@ const CountryItem: React.FC<CountryItemProps> = ({ country }) => {
   });
 
   return (
-    <li className="flex max-h-[300px] min-w-[292px] flex-col items-center gap-1 rounded border p-4 shadow-md">
+    <li
+      className={`${isSelectedCountry && "bg-green-300"} flex max-h-[300px] min-w-[292px] cursor-pointer flex-col items-center gap-1 rounded border p-4 shadow-md transition hover:scale-110 hover:border-green-300`}
+      onClick={() => handleSelectCountry(country)}
+    >
       <img src={imageUrl} className="h-[180px] w-full max-w-[260px]" />
       <h2 className="text-lg font-bold">{country.translations.kor.common}</h2>
       <h3>{country.name.common}</h3>
--- src/components/country/CountryList.tsx
+++ src/components/country/CountryList.tsx
@@ -1,5 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import api from "../../api/api";
+import { CountryResponse } from "../../api/country.type";
 import CountryItem from "./CountryItem";
 
 const CountryList = () => {
@@ -8,10 +10,37 @@ const CountryList = () => {
     queryFn: () => api.country.getAllCountry(),
   });
 
+  const [selectedCountry, setSelectedCountry] = useState<CountryResponse[]>([]);
+
+  const handleSelectCountry = (newCountry: CountryResponse) => {
+    const isSelectedCountry = selectedCountry.find(
+      (prevCountry) => prevCountry.name.common === newCountry.name.common
+    );
+
+    if (isSelectedCountry) {
+      setSelectedCountry(
+        selectedCountry.filter(
+          (prevCountry) => prevCountry.name.common !== newCountry.name.common
+        )
+      );
+    } else {
+      setSelectedCountry([...selectedCountry, newCountry]);
+    }
+  };
+
   return (
     <ul className="flex flex-wrap justify-center gap-4">
       {countryList.map((country) => (
-        <CountryItem key={country.name.common} country={country} />
+        <CountryItem
+          key={country.name.common}
+          country={country}
+          isSelectedCountry={
+            !!selectedCountry.find(
+              (prevCountry) => prevCountry.name.common === country.name.common
+            )
+          }
+          handleSelectCountry={handleSelectCountry}
+        />
       ))}
     </ul>
   );
